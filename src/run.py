e17e07bc07184aebc2a0ef758ebd9d04aa7babb7
ranpo02 <yassinsino444@gmail.com>
2025-12-28 19:38:45 +0100

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,13 +1,10 @@
-# run.py
-import sys
-import os
-
-# هذا السطر مهم جدًا ليتمكن بايثون من إيجاد الملفات داخل مجلد src
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-# الآن نستورد دالة التشغيل من src/bot.py
-from bot import run
+# src/run.py
+from bot import main
+import asyncio
 
 if __name__ == "__main__":
     print("بدء تشغيل البوت...")
-    run()
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("تم إيقاف البوت.")
